5c0bdb2926de7434be20cf46c18e8e2735ddffcb
ruslan <to@ufasg.com>
2020-12-18 13:58:33 +0500

    новый класс маин

--- src/main.java
+++ src/main.java
@@ -1,2 +1,9 @@
 public class main {
+    public static void main(String[] args) {
+        String str1 = "A";
+        String str2 = "A";
+        String str3 = "B";
+        System.out.println(str1.equals(str2));
+        System.out.println(str1.equals(str3));
+    }
 }
